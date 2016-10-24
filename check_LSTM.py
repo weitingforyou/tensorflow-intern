cb32725a3686c3d0cada42d6d0dba70eeb601f57
weitingforyou <liang0962054.dif02@g2.nctu.edu.tw>
2016-10-24 08:48:54 +0800

    Update check_LSTM.py

--- check_LSTM.py
+++ check_LSTM.py
@@ -1,60 +1,75 @@
+#-*- coding: cp950 -*-
+
 import tensorflow as tf
 import numpy as np
+from tensorflow.python.ops import rnn, rnn_cell
+
 import matplotlib.pyplot as plt
 
 H = 256 # number of hidden layer neurons
 D = 4 # input dimensionality
-C = 3 # ouput classes
+C = 3 # ouput classes 
+n_input = 2 # MNIST data input (img shape: 2*2)
+n_steps = 2 # timesteps
 
-# Define weights
-weights = {
-    'hidden': tf.Variable(tf.random_normal([D, H])),
-    'out': tf.Variable(tf.random_normal([H, C]))
-}
-biases = {
-    'hidden': tf.Variable(tf.random_normal([H,])),
-    'out': tf.Variable(tf.random_normal([C,]))
-}
+tf.reset_default_graph()
+
+def bias_variable(name, shape):
+    return tf.Variable(tf.constant(0.01, shape = shape, name = name))
 
 status = tf.placeholder(tf.float32, [None,D] , name="input_x")
 input_y = tf.placeholder(tf.float32,[None,C], name="input_y")
 advantages = tf.placeholder(tf.float32,name="reward_signal")
 
-def RNN(_status, _weights, _biases):
-    _status = tf.reshape(_status, [-1,D])
-    _status = tf.matmul(_status, _weights['hidden']) + _biases['hidden']
-    _status = tf.reshape(_status, [-1,1,H])
-    
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(H, forget_bias=1.0, state_is_tuple=True)
-    '''
-    If the version of your tensorflow is higher than r0.9, then your lstm_cell should try the following :
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(H, forget_bias=1.0, state_is_tuple=True)
-    '''
-    _init_state = lstm_cell.zero_state(10, dtype=tf.float32)
-    outputs, states = tf.nn.dynamic_rnn(lstm_cell, _status, initial_state=_init_state, time_major=False)
-    
-    outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2])) 
-    score = tf.matmul(outputs[-1], _weights['out']) + _biases['out']
-    
-    return score
+# Define weights
+#W1 = tf.get_variable("W1", shape=[D, H], initializer=tf.contrib.layers.xavier_initializer())
+#b1 = tf.Variable(tf.constant(0.01, shape = [H], name = "B1"))
 
-score = RNN(status, weights, biases)
+W2 = tf.get_variable("W2", shape=[H, C], initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.constant(0.01, shape = [C], name = "B2"))
+
+status1 = tf.reshape(status, [-1,n_steps, n_input])
+status2 = tf.transpose(status1, [1, 0, 2])
+status3 = tf.reshape(status2, [-1, n_input])
+status4 = tf.split(0, n_steps, status3)
+
+lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(H, forget_bias=1.0)
+
+outputs, states = rnn.rnn(lstm_cell, status4, dtype=tf.float32)
+
+score = tf.matmul(outputs[-1], W2) + b2
+
+'''
+W1 = tf.get_variable("W1", shape=[D, H],
+           initializer=tf.contrib.layers.xavier_initializer())
+b1 = bias_variable("B1", [H]) 
+layer1 = tf.nn.relu(tf.matmul(status,W1) + b1)
+W2 = tf.get_variable("W2", shape=[H, H*2],
+           initializer=tf.contrib.layers.xavier_initializer())
+b2 = bias_variable("B2", [H*2]) 
+layer2 = tf.nn.relu(tf.matmul(layer1,W2) + b2)
+W3 = tf.get_variable("W3", shape=[H*2, 3],
+           initializer=tf.contrib.layers.xavier_initializer())        
+         
+score = tf.matmul(layer2, W3) 
+'''
+
 loss = tf.reduce_sum(tf.square(input_y*advantages - score)) 
+
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss) 
 
 all_status = [1,2,3,4],[2,3,4,5],[3,4,5,6],[4,5,6,7],[5,6,7,8],[6,7,8,9],[7,8,9,10],[8,9,10,11],[9,10,11,12],[10,11,12,13]
 all_action = [1,0,0],  [0,1,0],  [0,0,1],  [1,0,0],  [0,1,0],  [0,0,1],  [1,0,0],   [0,1,0],    [0,0,1],     [1,0,0]
 all_reward = [1,        -2,        3,       -4,        5,       -6,       -7,        8,           -9,         10]
-
+    
 episode_number = 0
-total_episodes = 10000
+total_episodes = 500
 init = tf.initialize_all_variables()
 
 all_status = np.array(all_status)
 all_action = np.vstack(all_action)
 all_reward = np.vstack(all_reward)
 my_list = []
-
 
 # Launch the graph
 with tf.Session() as sess:
@@ -70,22 +85,21 @@ with tf.Session() as sess:
         print 'all_action*all_reward:', all_action*all_reward
       
         p1 = sess.run(score,feed_dict={status: all_status})
-        #print episode_number, 'p1:', p1
+        print episode_number, 'p1:', p1
         
         sess.run(optimizer ,feed_dict={status: all_status, input_y: all_action, advantages: all_reward})  
         
         p2 = sess.run(score,feed_dict={status: all_status})        
         print episode_number, 'p2:', p2
         
-        #print 'p2-p1:', p2-p1
+        print 'p2-p1:', p2-p1
         
         print 'loss:', sess.run(loss ,feed_dict={status: all_status, input_y: all_action, advantages: all_reward})  
-
-        #print 'all_action*all_reward:', all_action*all_reward
-        #print 'abs(all_action*all_reward-p2):', abs(all_action*all_reward-p2)
+        
+        print 'all_action*all_reward:', all_action*all_reward
+        print 'abs(all_action*all_reward-p2):', abs(all_action*all_reward-p2)
         print 'np.sum(abs(all_action*all_reward-p2):', np.sum(abs(all_action*all_reward-p2))
         my_list.append(np.sum(abs(all_action*all_reward-p2)))
-        
         
         if (episode_number % 20 == 0):
             # ref : https://github.com/MorvanZhou/tutorials/blob/master/tensorflowTUT/tensorflow12_plut_result.py
@@ -100,6 +114,5 @@ with tf.Session() as sess:
             plt.ylim(np.min(new_my_list)*0.95,np.max(new_my_list)*1.05)
             lines  = ax.plot(x_list, new_my_list)
             plt.pause(0.01)
-         
-plt.pause(60)
 
+plt.pause(60)
